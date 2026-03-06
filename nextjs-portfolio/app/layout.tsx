7cc6417d0d2b07e4d8756ccc063b234c63d86e0b
sudarshanmestha <sudarshan15399@gmail.com>
2026-03-06 12:40:57 +0530

    index page chages made

--- nextjs-portfolio/app/layout.tsx
+++ nextjs-portfolio/app/layout.tsx
@@ -1,13 +1,15 @@
+// app/layout.tsx
+import { AuthProvider } from "@/lib/AuthContext"; // MUST be this exact path
+import Navbar from './components/nav';
 import './global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import Navbar from './components/nav' 
 import Footer from './components/footer'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { baseUrl } from './sitemap'
-import { AuthProvider } from "@/lib/AuthContext"; 
+
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
